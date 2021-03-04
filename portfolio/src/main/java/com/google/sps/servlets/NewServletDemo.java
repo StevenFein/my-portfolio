bd220339021f12c5bc0c3c3457ccdef58594a35f
Steven Fein <saf0929@gmail.com>
2021-03-04 23:51:10 +0000

    Add NewServletDemo.java file. Add link to /demo in index.html

---
+++ portfolio/src/main/java/com/google/sps/servlets/NewServletDemo.java
@@ -0,0 +1,18 @@
+package com.google.sps.servlets;
+
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/** Handles requests sent to the /demo URL. Try running a server and navigating to /demo! */
+@WebServlet("/demo")
+public class NewServletDemo extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    response.setContentType("text/html;");
+    response.getWriter().println("<h1>If you can see this, then the servlet demo worked!</h1>");
+  }
+}
